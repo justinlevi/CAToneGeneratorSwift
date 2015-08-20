ffdfb519dffd09b461e4e31b97a81f3cf4ac5640
Justin Winter <justinlevi@gmail.com>
2015-08-20 12:49:15 -0400

    swift 2 update

---
+++ CAToneFileGeneratorSwift/AudioError.swift
@@ -0,0 +1,9 @@
+//
+//  AudioError.swift
+//  CAToneFileGeneratorSwift
+//
+//  Created by Justin Winter on 7/2/15.
+//  Copyright © 2015 wintercreative. All rights reserved.
+//
+
+import Foundation
