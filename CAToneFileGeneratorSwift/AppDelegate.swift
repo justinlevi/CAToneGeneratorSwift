16e34ad684f827d172b058c50361f2df299f201e
Justin Winter <justinlevi@gmail.com>
2015-08-20 12:49:23 -0400

    swift 2 update

--- CAToneFileGeneratorSwift/AppDelegate.swift
+++ CAToneFileGeneratorSwift/AppDelegate.swift
@@ -6,60 +6,22 @@
 //  Copyright (c) 2015 wintercreative. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import AudioToolbox
-import Foundation
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
-  
-  func CheckError(error:OSStatus) -> String? {
-    if error == 0 { return nil}
-  
-    switch(error) {
-    case kAUGraphErr_NodeNotFound : return("Error:kAUGraphErr_NodeNotFound \n");
-    case kAUGraphErr_OutputNodeErr : return( "Error:kAUGraphErr_OutputNodeErr \n");
-    case kAUGraphErr_InvalidConnection : return("Error:kAUGraphErr_InvalidConnection \n");
-    case kAUGraphErr_CannotDoInCurrentContext : return( "Error:kAUGraphErr_CannotDoInCurrentContext \n");
-    case kAUGraphErr_InvalidAudioUnit : return( "Error:kAUGraphErr_InvalidAudioUnit \n");
-    case kAudioToolboxErr_InvalidSequenceType : return( " kAudioToolboxErr_InvalidSequenceType ");
-    case kAudioToolboxErr_TrackIndexError : return( " kAudioToolboxErr_TrackIndexError ");
-    case kAudioToolboxErr_TrackNotFound : return( " kAudioToolboxErr_TrackNotFound ");
-    case kAudioToolboxErr_EndOfTrack : return( " kAudioToolboxErr_EndOfTrack ");
-    case kAudioToolboxErr_StartOfTrack : return( " kAudioToolboxErr_StartOfTrack ");
-    case kAudioToolboxErr_IllegalTrackDestination	: return( " kAudioToolboxErr_IllegalTrackDestination");
-    case kAudioToolboxErr_NoSequence : return( " kAudioToolboxErr_NoSequence ");
-    case kAudioToolboxErr_InvalidEventType : return( " kAudioToolboxErr_InvalidEventType");
-    case kAudioToolboxErr_InvalidPlayerState : return( " kAudioToolboxErr_InvalidPlayerState");
-    case kAudioUnitErr_InvalidProperty : return( " kAudioUnitErr_InvalidProperty");
-    case kAudioUnitErr_InvalidParameter	: return( " kAudioUnitErr_InvalidParameter");
-    case kAudioUnitErr_InvalidElement	: return( " kAudioUnitErr_InvalidElement");
-    case kAudioUnitErr_NoConnection	: return( " kAudioUnitErr_NoConnection");
-    case kAudioUnitErr_FailedInitialization	: return( " kAudioUnitErr_FailedInitialization");
-    case kAudioUnitErr_TooManyFramesToProcess	: return( " kAudioUnitErr_TooManyFramesToProcess");
-    case kAudioUnitErr_InvalidFile : return( " kAudioUnitErr_InvalidFile");
-    case kAudioUnitErr_FormatNotSupported	: return( " kAudioUnitErr_FormatNotSupported");
-    case kAudioUnitErr_Uninitialized : return( " kAudioUnitErr_Uninitialized");
-    case kAudioUnitErr_InvalidScope	: return( " kAudioUnitErr_InvalidScope");
-    case kAudioUnitErr_PropertyNotWritable	: return( " kAudioUnitErr_PropertyNotWritable");
-    case kAudioUnitErr_InvalidPropertyValue	: return( " kAudioUnitErr_InvalidPropertyValue");
-    case kAudioUnitErr_PropertyNotInUse	: return( " kAudioUnitErr_PropertyNotInUse");
-    case kAudioUnitErr_Initialized : return( " kAudioUnitErr_Initialized");
-    case kAudioUnitErr_InvalidOfflineRender	: return( " kAudioUnitErr_InvalidOfflineRender");
-    case kAudioUnitErr_Unauthorized	: return( " kAudioUnitErr_Unauthorized");
-    default: return("huh?")
-    }
-  }
-
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     
     let SAMPLE_RATE:Float64 = 44100                                       // 1
     let DURATION = 2.0                                                    // 2
-    let HZ:Double = 440
-    let FILE_NAME = "\(HZ)-saw.aif"                                      // 3
+    let HZ:Double = 440 // Middle A or Concert A - Number of cycles per second
+    let FILE_NAME = "\(HZ)-sine.aif"                                      // 3
     
     let fileManager = NSFileManager.defaultManager()
     let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
@@ -70,26 +32,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       
       // Prepare the format
       var audioFormat = AudioStreamBasicDescription()                     // 6
-      audioFormat.mSampleRate       = SAMPLE_RATE;                        // 8
+      audioFormat.mSampleRate       = SAMPLE_RATE; //44,100               // 8
       audioFormat.mFormatID         = UInt32(kAudioFormatLinearPCM)
+      audioFormat.mBitsPerChannel   = UInt32(16) //16-bit sample
+      audioFormat.mChannelsPerFrame = 1 // Mono
+      audioFormat.mFramesPerPacket  = 1 // LPCM Doesn't use packets
+      audioFormat.mBytesPerFrame    = UInt32(audioFormat.mChannelsPerFrame * 2) // each frame has 2 bytes
+      audioFormat.mBytesPerPacket   = audioFormat.mFramesPerPacket * audioFormat.mBytesPerFrame
       audioFormat.mFormatFlags      = UInt32(kLinearPCMFormatFlagIsBigEndian) |
                                       UInt32(kLinearPCMFormatFlagIsSignedInteger) |
                                       UInt32(kLinearPCMFormatFlagIsPacked)
-      audioFormat.mBitsPerChannel   = UInt32(16)
-      audioFormat.mChannelsPerFrame = 1 // Mono
-      audioFormat.mBytesPerFrame    = UInt32(audioFormat.mChannelsPerFrame * 2)
-      audioFormat.mFramesPerPacket  = 1
-      audioFormat.mBytesPerPacket   = audioFormat.mFramesPerPacket * audioFormat.mBytesPerFrame
+      //NOTE: mFormatFlags varies based on the format you're using
       
       var audioFile:AudioFileID = nil
       var theErr = OSStatus(noErr)
                                                                           // 9
       theErr = AudioFileCreateWithURL(fileURL, UInt32(kAudioFileAIFFType), &audioFormat, .EraseFile, &audioFile)
-      if theErr != 0 { print(CheckError(theErr)) }
+      
+      if let err = AudioError(err: theErr) { print(err) }
       
       let maxSampleCount = CLong(SAMPLE_RATE * DURATION)                  // 10
       var sampleCount = 0
       var bytesToWrite:UInt32 = 2
+      
+      //44,100 Hz / 440 Hz = 100.22
       let wavelengthInSamples = SAMPLE_RATE / HZ                          // 11
       
       while sampleCount < maxSampleCount {
@@ -98,21 +64,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
           // Square Wave
           //var sample = Int16(i < Int(wavelengthInSamples) / 2 ? Int16.max : Int16.min).bigEndian //12 & 13
           
-          // Saw Tooth : Kind of...
-          var sample = Int16(((Double(i) / wavelengthInSamples) * Double(Int16.max) * 2) - Double(Int16.max)).bigEndian
+          // Saw Tooth
+          // var sample = Int16(((Double(i) / wavelengthInSamples) * Double(Int16.max) * 2) - Double(Int16.max)).bigEndian
           
           // Sine Sample
-          //var sample = Int16(Double(Int16.max) * sin(2 * M_PI * (Double(i) / wavelengthInSamples))).bigEndian
+          var sample = Int16(Double(Int16.max) * sin(2 * M_PI * (Double(i) / wavelengthInSamples))).bigEndian
           
-          theErr = AudioFileWriteBytes(audioFile, 0, Int64(sampleCount * 2), &bytesToWrite, &sample)
-          if theErr != 0 { print(CheckError(theErr)) }
+          theErr = AudioFileWriteBytes(audioFile, false, Int64(sampleCount * 2), &bytesToWrite, &sample)
+          if let err = AudioError.errorFromOSStatus(theErr) { print(err) }
           
           sampleCount++
                                                                           // 15
         }
       }
       theErr = AudioFileClose(audioFile)                                  // 16
-      if theErr != 0 { print(CheckError(theErr)) }
+      if let err = AudioError.errorFromOSStatus(theErr) { print(err) }
     }
     
     return true
--- CAToneFileGeneratorSwift/AudioError.swift
+++ CAToneFileGeneratorSwift/AudioError.swift
@@ -7,3 +7,88 @@
 //
 
 import Foundation
+import AudioToolbox
+
+enum AudioError: String, ErrorType {
+  case NodeNotFound             	= "NodeNotFound"
+  case OutputNodeErr            	= "OutputNodeErr"
+  case InvalidConnection        	= "InvalidConnection"
+  case CannotDoInCurrentContext 	= "CannotDoInCurrentContext"
+  case InvalidAudioUnit         	= "InvalidAudioUnit"
+  case InvalidSequenceType      	= "InvalidSequenceType"
+  case TrackIndexError          	= "TrackIndexError"
+  case TrackNotFound            	= "TrackNotFound"
+  case EndOfTrack               	= "EndOfTrack"
+  case StartOfTrack             	= "StartOfTrack"
+  case IllegalTrackDestination  	= "IllegalTrackDestination"
+  case NoSequence               	= "NoSequence"
+  case InvalidEventType         	= "InvalidEventType"
+  case InvalidPlayerState       	= "InvalidPlayerState"
+  case InvalidProperty          	= "InvalidProperty"
+  case InvalidParameter         	= "InvalidParameter"
+  case InvalidElement           	= "InvalidElement"
+  case NoConnection             	= "NoConnection"
+  case FailedInitialization     	= "FailedInitialization"
+  case TooManyFramesToProcess   	= "TooManyFramesToProcess"
+  case InvalidFile              	= "InvalidFile"
+  case FormatNotSupported       	= "FormatNotSupported"
+  case Uninitialized            	= "Uninitialized"
+  case InvalidScope             	= "InvalidScope"
+  case PropertyNotWritable      	= "PropertyNotWritable"
+  case InvalidPropertyValue     	= "InvalidPropertyValue"
+  case PropertyNotInUse         	= "PropertyNotInUse"
+  case Initialized              	= "Initialized"
+  case InvalidOfflineRender     	= "InvalidOfflineRender"
+  case Unauthorized             	= "Unauthorized"
+  case Unknown                  	= "Unknown"
+  
+  
+  init?(err: OSStatus){
+    if let ae = AudioError.errorFromOSStatus(err) {
+      self = ae
+    }else{
+      return nil
+    }
+  }
+  
+  
+  /// Returns the appropriate error for the sattus, or nil if successful
+  /// or unknown for a code that doesn't match
+  static func errorFromOSStatus(rawStatus:OSStatus) -> AudioError? {
+    switch rawStatus {
+    case 0 : return nil
+    case kAUGraphErr_NodeNotFound : return .NodeNotFound
+    case kAUGraphErr_NodeNotFound : return .NodeNotFound
+    case kAUGraphErr_OutputNodeErr : return .OutputNodeErr
+    case kAUGraphErr_InvalidConnection : return .InvalidConnection
+    case kAUGraphErr_CannotDoInCurrentContext : return .CannotDoInCurrentContext
+    case kAUGraphErr_InvalidAudioUnit : return .InvalidAudioUnit
+    case kAudioToolboxErr_InvalidSequenceType : return .InvalidSequenceType
+    case kAudioToolboxErr_TrackIndexError : return .TrackIndexError
+    case kAudioToolboxErr_TrackNotFound : return .TrackNotFound
+    case kAudioToolboxErr_EndOfTrack : return .EndOfTrack
+    case kAudioToolboxErr_StartOfTrack : return .StartOfTrack
+    case kAudioToolboxErr_IllegalTrackDestination	: return .IllegalTrackDestination
+    case kAudioToolboxErr_NoSequence : return .NoSequence
+    case kAudioToolboxErr_InvalidEventType : return .InvalidEventType
+    case kAudioToolboxErr_InvalidPlayerState : return .InvalidPlayerState
+    case kAudioUnitErr_InvalidProperty : return .InvalidProperty
+    case kAudioUnitErr_InvalidParameter	: return .InvalidParameter
+    case kAudioUnitErr_InvalidElement	: return .InvalidElement
+    case kAudioUnitErr_NoConnection	: return .NoConnection
+    case kAudioUnitErr_FailedInitialization	: return .FailedInitialization
+    case kAudioUnitErr_TooManyFramesToProcess	: return .TooManyFramesToProcess
+    case kAudioUnitErr_InvalidFile : return .InvalidFile
+    case kAudioUnitErr_FormatNotSupported	: return .FormatNotSupported
+    case kAudioUnitErr_Uninitialized : return .Uninitialized
+    case kAudioUnitErr_InvalidScope	: return .InvalidScope
+    case kAudioUnitErr_PropertyNotWritable	: return .PropertyNotWritable
+    case kAudioUnitErr_InvalidPropertyValue	: return .InvalidPropertyValue
+    case kAudioUnitErr_PropertyNotInUse	: return .PropertyNotInUse
+    case kAudioUnitErr_Initialized : return .Initialized
+    case kAudioUnitErr_InvalidOfflineRender	: return .InvalidOfflineRender
+    case kAudioUnitErr_Unauthorized	: return .Unauthorized
+    default: return .Unknown
+    }
+  }
+}
